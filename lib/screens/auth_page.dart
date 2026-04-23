a6be596cd83519fa1b083389c9d5f5d2b44a5b81
sadam <sadamirmar@gmail.com>
2026-04-23 19:03:44 +0700

    buat auth page

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/auth_provider.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  State<AuthPage> createState() => _AuthPageState();
+}
+
+class _AuthPageState extends State<AuthPage> {
+  bool isLogin = true;
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
 
