424b79ff80509392d2656494fe50420e0b736a34
sadam <sadamirmar@gmail.com>
2026-04-23 19:06:22 +0700

    membuat header auth page

--- lib/screens/auth_page.dart
+++ lib/screens/auth_page.dart
@@ -13,3 +13,47 @@ class _AuthPageState extends State<AuthPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  @override
+  Widget build(BuildContext context) {
+    final auth = context.watch<AuthProvider>();
+
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.black, Colors.brown.shade900],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Icon(
+                  Icons.local_fire_department,
+                  size: 80,
+                  color: Colors.orange,
+                ),
+
+                SizedBox(height: 10),
+
+                Text(
+                  "SMOKER SHOP",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2,
+                  ),
+                ),
+
+                SizedBox(height: 30),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
