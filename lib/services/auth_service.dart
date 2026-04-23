c97f2dada9465d44f80d5075b3deabc95f8096ba
sadam <sadamirmar@gmail.com>
2026-04-23 18:25:51 +0700

    bikin auth service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> register(String email, String password) async {
+    final result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
