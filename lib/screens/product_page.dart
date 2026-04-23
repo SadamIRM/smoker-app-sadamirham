29f01405ca824a2438e8a123f3f3bfd4080937d8
sadam <sadamirmar@gmail.com>
2026-04-24 06:01:47 +0700

    bikin snapshots untuk baca data dari firestore di product page

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -5,8 +5,9 @@ import '../models/product.dart';
 import '../providers/cart_provider.dart';
 
 class ProductPage extends StatelessWidget {
-  final CollectionReference productsRef =
-      FirebaseFirestore.instance.collection('products');
+  final CollectionReference productsRef = FirebaseFirestore.instance.collection(
+    'products',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -37,12 +38,79 @@ class ProductPage extends StatelessWidget {
                     style: const TextStyle(fontSize: 10),
                   ),
                 ),
-              )
+              ),
             ],
-          )
+          ),
         ],
       ),
-    
+      body: StreamBuilder<QuerySnapshot>(
+        stream: productsRef.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: Text("No Data"));
+          }
+
+          final docs = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              final product = Product.fromFirestore(docs[index]);
+
+              return Container(
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.white10,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(color: Colors.orangeAccent),
+                ),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.smoking_rooms,
+                      color: Colors.orangeAccent,
+                      size: 40,
+                    ),
+
+                    const SizedBox(width: 15),
+
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            "Rp ${product.price}",
+                            style: const TextStyle(color: Colors.white70),
+                          ),
+                        ],
+                      ),
+                    ),
+
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.brown,
+                      ),
+                      onPressed: () {
+                        context.read<CartProvider>().addToCart(product);
+                      },
+                      child: const Text("Add"),
+                    ),
+                  ],
+                ),
+              );
+            },
           );
         },
       ),
