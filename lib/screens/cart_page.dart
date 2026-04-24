b824eb1ebcc943c5e9a55d96b892d6ce0c695438
sadam <sadamirmar@gmail.com>
2026-04-24 08:14:31 +0700

    bikin list item

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -20,5 +20,48 @@ class CartPage extends StatelessWidget {
                 style: TextStyle(color: Colors.white70),
               ),
             )
-     
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: cart.items.length,
+                    itemBuilder: (context, index) {
+                      final item = cart.items[index];
+
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 6,
+                        ),
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.white10,
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.orangeAccent),
+                        ),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.smoking_rooms,
+                              color: Colors.orangeAccent,
+                            ),
+
+                            const SizedBox(width: 10),
+
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item.name,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    "Rp ${item.price}",
+                                    style: const TextStyle(
+                                      color: Colors.white70,
+                                    ),
+                                  ),
+                                ],
+                              ),
           
