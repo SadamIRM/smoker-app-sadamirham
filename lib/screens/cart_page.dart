5714bd5247b89f52d66326d51362970d5cd7320c
sadam <sadamirmar@gmail.com>
2026-04-24 08:14:49 +0700

    bikin checkout di cart

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -64,4 +64,66 @@ class CartPage extends StatelessWidget {
                                   ),
                                 ],
                               ),
-          
+                            ),
+
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () {
+                                cart.removeFromCart(item);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Colors.white10,
+                    border: Border(top: BorderSide(color: Colors.white24)),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Total: Rp ${cart.totalPrice}",
+                        style: const TextStyle(
+                          color: Colors.orangeAccent,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                      SizedBox(
+                        width: double.infinity,
+                        height: 45,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                          ),
+                          onPressed: () {
+                            cart.clearCart();
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text("Checkout berhasil (dummy)"),
+                              ),
+                            );
+                          },
+                          child: const Text("Checkout"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
