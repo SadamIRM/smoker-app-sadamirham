7a80ad67aabb486cf456d728af2ea78518d22997
sadam <sadamirmar@gmail.com>
2026-04-24 08:07:14 +0700

    rombak main kelar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
+
 import 'providers/auth_provider.dart';
+import 'providers/cart_provider.dart';
 import 'screens/auth_wrapper.dart';
+import 'screens/cart_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +22,15 @@ void main() async {
     ),
   );
 
-  runApp(ChangeNotifierProvider(create: (_) => AuthProvider(), child: MyApp()));
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CartProvider()),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -32,7 +43,10 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.brown,
         scaffoldBackgroundColor: Colors.black,
       ),
+
       home: AuthWrapper(),
+
+      routes: {'/cart': (context) => CartPage()},
     );
   }
 }
