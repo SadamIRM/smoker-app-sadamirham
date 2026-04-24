d52d2e8d66fe84e721b86a238dfe5a7e677c13a5
sadam <sadamirmar@gmail.com>
2026-04-24 08:52:14 +0700

    rombak cart page

--- lib/screens/cart_page.dart
+++ lib/screens/cart_page.dart
@@ -33,51 +33,7 @@ class CartPage extends StatelessWidget {
                           horizontal: 12,
                           vertical: 6,
                         ),
-                        padding: const EdgeInsets.all(12),
-                        decoration: BoxDecoration(
-                          color: Colors.white10,
-                          borderRadius: BorderRadius.circular(12),
-                          border: Border.all(color: Colors.orangeAccent),
-                        ),
-                        child: Row(
-                          children: [
-                            const Icon(
-                              Icons.smoking_rooms,
-                              color: Colors.orangeAccent,
-                            ),
-
-                            const SizedBox(width: 10),
-
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    item.name,
-                                    style: const TextStyle(color: Colors.white),
-                                  ),
-                                  Text(
-                                    "Rp ${item.price}",
-                                    style: const TextStyle(
-                                      color: Colors.white70,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-
-                            IconButton(
-                              icon: const Icon(Icons.delete, color: Colors.red),
-                              onPressed: () {
-                                cart.removeFromCart(item);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                ),
+             
 
                 Container(
                   padding: const EdgeInsets.all(20),
